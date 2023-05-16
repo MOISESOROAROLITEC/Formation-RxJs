8f586b3e980613703f5d2f274f92065e7f6a1ef0
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-05-16 19:39:27 +0000

    finished basic countdown

---
+++ index.js
@@ -0,0 +1,24 @@
+import { from, interval } from "rxjs";
+import { filter, map, scan } from "rxjs/operators";
+
+const number = [1, 2, 3, 4, 5];
+
+const countdown = document.getElementsByClassName('countdown')[0]
+const message = document.getElementsByClassName('message')[0]
+
+const subscroptor = interval(500).pipe(
+	map(() => -1),
+
+	scan((accumulator, value) => {
+		return accumulator + value
+	}, 11),
+
+	filter(val => val >= 0)
+
+)
+
+subscroptor.subscribe((val) => {
+	countdown.innerHTML = val;
+	if (!val)
+		message.innerHTML = "Countdown finished !"
+});
