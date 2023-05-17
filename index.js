3f0454972f640ed5ea77046228c8f0fc6a90dafc
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-05-17 16:07:44 +0000

    end of project

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-import { from, interval } from "rxjs";
-import { filter, map, scan } from "rxjs/operators";
+import { from, fromEvent, interval } from "rxjs";
+import { filter, map, scan, takeUntil, takeWhile, tap } from "rxjs/operators";
 
 const number = [1, 2, 3, 4, 5];
 
 const countdown = document.getElementsByClassName('countdown')[0]
 const message = document.getElementsByClassName('message')[0]
+const stopBtn = document.getElementsByClassName('stop_btn')[0]
+const stopClic = fromEvent(stopBtn, "click")
 
-const subscroptor = interval(1000).pipe(
+const subscroptor = interval(400).pipe(
 	map(() => -1),
 
 	scan((accumulator, value) => {
 		return accumulator + value
 	}, 11),
-
-	filter(val => val >= 0)
-
+	takeUntil(stopClic),
+	takeWhile(el => el >= 0),
 )
 
 subscroptor.subscribe((val) => {
 	countdown.innerHTML = val;
-	if (!val)
+	if (!val) {
+		stopBtn.remove()
 		message.innerHTML = "Countdown finished !"
+	};
+
 });
