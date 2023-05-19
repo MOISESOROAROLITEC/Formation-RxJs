36e2d1e33f8774199397f7b695f10b3b3df578ab
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-05-19 16:09:16 +0000

    finish switchMap

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-import { fromEvent, interval } from "rxjs";
-import { map, mergeMap, takeUntil } from "rxjs/operators";
+import { fromEvent } from "rxjs";
+import { debounceTime, distinctUntilChanged, map, switchMap } from "rxjs/operators";
+import { ajax } from 'rxjs/ajax'
 
-const click$ = fromEvent(document, "click");
-const mousedown$ = fromEvent(document, 'mousedown');
-const mouseup$ = fromEvent(document, 'mouseup');
-const interval$ = interval(500);
+const inputElement = document.getElementById(
+	"input-text"
+);
+const listItem = document.getElementById("list_item")
+const BASE_URL = "https://api.openbrewerydb.org/breweries";
 
-mousedown$.pipe(
+const input$ = fromEvent(inputElement, "keyup");
 
-	mergeMap(() => interval$.pipe(
-		takeUntil(mouseup$)
-	))
+
+input$.pipe(
+
+	debounceTime(1000),
+	map(el => el.target.value),
+	distinctUntilChanged(),
+	switchMap(searchTerm => {
+		return ajax.getJSON(
+			`${BASE_URL}?by_name=${searchTerm}`
+		)
+	})
 
 ).subscribe({
-	next: el => console.log("el est : ", el),
+	next: response => {
+		listItem.innerHTML = response.map(el => `<div class="el">${el.name}</div>`).join("<br/>")
+	},
 	error: er => console.log("l'erreur est : ", er),
 	complete: () => console.log("Fin du traitement")
 })
