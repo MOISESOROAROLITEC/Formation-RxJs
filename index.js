81d366268a111140463ba48b66336f9c120e851d
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-05-20 14:11:57 +0000

    init catch error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { fromEvent } from "rxjs";
-import { debounceTime, distinctUntilChanged, map, switchMap } from "rxjs/operators";
+import { concatMap, debounceTime, distinctUntilChanged, map } from "rxjs/operators";
 import { ajax } from 'rxjs/ajax'
 
 const inputElement = document.getElementById(
@@ -13,10 +13,10 @@ const input$ = fromEvent(inputElement, "keyup");
 
 input$.pipe(
 
-	debounceTime(1000),
+	debounceTime(700),
 	map(el => el.target.value),
 	distinctUntilChanged(),
-	switchMap(searchTerm => {
+	concatMap(searchTerm => {
 		return ajax.getJSON(
 			`${BASE_URL}?by_name=${searchTerm}`
 		)
@@ -24,7 +24,11 @@ input$.pipe(
 
 ).subscribe({
 	next: response => {
-		listItem.innerHTML = response.map(el => `<div class="el">${el.name}</div>`).join("<br/>")
+		if (response.length == 0) {
+			listItem.innerHTML = "<div class='empty'>Not found</div>"
+		} else {
+			listItem.innerHTML = response.map(el => `<div class="line"><div class="el">${el.name}</div><div class="el_type">${el.brewery_type}</div></div>`).join("")
+		}
 	},
 	error: er => console.log("l'erreur est : ", er),
 	complete: () => console.log("Fin du traitement")
