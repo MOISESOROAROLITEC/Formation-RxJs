fe9dc1db0e29b8dd92f1251ef6649c9b6e56ebb2
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-05-22 12:05:45 +0000

    init merge branche

--- index.js
+++ index.js
@@ -1,46 +1,13 @@
-import { from, fromEvent, interval } from "rxjs";
-import { endWith, exhaustMap, filter, map, scan, skipUntil, startWith, takeUntil, takeWhile, tap } from "rxjs/operators";
+import { from, fromEvent, interval, merge } from "rxjs";
+import { delay, endWith, exhaustMap, filter, map, scan, skipUntil, startWith, takeUntil, takeWhile, tap } from "rxjs/operators";
 
-const number = [1, 2, 3, 4, 5];
+const click$ = fromEvent(document, "click");
+const keyup$ = fromEvent(document, "keyup");
 
-const countdown = document.getElementsByClassName('countdown')[0]
-const message = document.getElementsByClassName('message')[0]
-const startBtn = document.getElementById('start-btn');
-const stopBtn = document.getElementById('stop-btn');
+// click$.subscribe(console.log)
+// keyup$.subscribe(console.log)
 
-const startBtn$ = fromEvent(startBtn, 'click');
-const stopBtn$ = fromEvent(stopBtn, 'click');
-
-const COUNTDOWN_FROM = 10
-
-countdown.innerHTML = COUNTDOWN_FROM;
-
-startBtn$.pipe(
-	exhaustMap(() =>
-		interval(1000).pipe(
-
-			map(() => -1),
-
-			tap(() => console.log("object")),
-
-			scan((accumulator, value) => {
-				return accumulator + value
-			}, COUNTDOWN_FROM),
-
-			takeWhile(el => el >= 0),
-
-			startWith(COUNTDOWN_FROM),
-
-			takeUntil(stopBtn$)
-		)
-	)
-).subscribe((val) => {
-	if (!val) {
-		message.style.display = "grid"
-		message.innerHTML = "Countdown finished !"
-		countdown.innerHTML = val;
-	} else {
-		countdown.innerHTML = val;
-		message.style.display = "none"
-	}
-});
+merge(
+	click$.pipe(delay(1000)),
+	keyup$
+).subscribe(console.log)
