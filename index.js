7e1f8f039b5be811b047da9174895a848dc26e3f
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-05-22 09:20:41 +0000

    update btn color and change mergeMapTo to exhaustMap

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import { fromEvent, interval, of, timer } from "rxjs"
-import { ajax } from 'rxjs/ajax'
-import { mergeMap, mergeMapTo, takeUntil, finalize, tap, switchMap, switchMapTo, map } from "rxjs/operators"
+import { takeUntil, finalize, tap, map, exhaustMap } from "rxjs/operators"
 
 const status = document.getElementById("status")
 const start_btn = document.getElementById("start-btn")
 const stop_btn = document.getElementById("stop-btn")
 const image = document.getElementById("dog-image")
-
-const URL = "https://random.dog/woof.json"
 
 const start$ = fromEvent(start_btn, 'click');
 const stop$ = fromEvent(stop_btn, 'click');
@@ -18,20 +15,14 @@ function getRandomImage() {
 }
 
 
-
-
 start$.pipe(
-	mergeMapTo(
-		timer(10, 1000).pipe(
+	exhaustMap(() =>
+		timer(10, 2000).pipe(
 
 			tap(() => {
 				status.innerHTML = "Starting"
 				status.className = "polling-status-started"
 			}),
-
-			// switchMapTo(
-			// 	ajax.get(URL)
-			// ),
 
 			map(() => getRandomImage()),
 
