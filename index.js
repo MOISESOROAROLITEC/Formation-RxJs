fd057ccfae75fdafc59c0f2f531ea80fe6600c79
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-05-22 08:48:26 +0000

    finish random dog

--- index.js
+++ index.js
@@ -1,52 +1,47 @@
-import { fromEvent } from "rxjs";
-import { catchError, concatMap, debounceTime, distinctUntilChanged, map } from "rxjs/operators";
+import { fromEvent, interval, of, timer } from "rxjs"
 import { ajax } from 'rxjs/ajax'
+import { mergeMap, mergeMapTo, takeUntil, finalize, tap, switchMap, switchMapTo, map } from "rxjs/operators"
 
-const inputElement = document.getElementById(
-	"input-text"
-);
-const listItem = document.getElementById("list_item");
-const BASE_URL = "https://api.openbrewerydb.org/breweries";
-// const reload = document.getElementById("reload");
+const status = document.getElementById("status")
+const start_btn = document.getElementById("start-btn")
+const stop_btn = document.getElementById("stop-btn")
+const image = document.getElementById("dog-image")
 
-// reload.addEventListener("click", () => {
-// 	location.reload();
-// })
+const URL = "https://random.dog/woof.json"
 
-const input$ = fromEvent(inputElement, "keyup");
+const start$ = fromEvent(start_btn, 'click');
+const stop$ = fromEvent(stop_btn, 'click');
+
+function getRandomImage() {
+	const randomNumber = Math.floor(Math.random() * 11);
+	return `./src/images/dog${randomNumber}.jpeg`;
+}
 
 
-input$.pipe(
-	debounceTime(700),
-	map(el => el.target.value),
-	distinctUntilChanged(),
-	concatMap(searchTerm => {
-		return ajax.getJSON(
-			`${BASE_URL}?by_name=${searchTerm}`
-		).pipe(
-			catchError((error, caught) => {
-				console.log({ error, caught })
-				return { error, caught }
+
+
+start$.pipe(
+	mergeMapTo(
+		timer(10, 1000).pipe(
+
+			tap(() => {
+				status.innerHTML = "Starting"
+				status.className = "polling-status-started"
+			}),
+
+			// switchMapTo(
+			// 	ajax.get(URL)
+			// ),
+
+			map(() => getRandomImage()),
+
+			takeUntil(stop$),
+
+			finalize(() => {
+				status.innerHTML = "Stoped"
+				status.className = "polling-status-stoped"
 			})
+
 		)
-	})
-
-).subscribe({
-	next: response => {
-		console.log(response);
-		if (response.length == 0) {
-			listItem.innerHTML = "<div class='empty'>Not found</div>"
-		} else {
-			listItem.innerHTML = response.map(el => `<div class="line"><div class="el">${el.name}</div><div class="el_type">${el.brewery_type}</div></div>`).join("")
-		}
-	},
-	error: er => {
-		listItem.innerHTML = "<div class='empty'>Erreur de connection, veillez recharger la page</div><button id='reload'>reload this page</button>";
-
-		const reload = document.getElementById("reload");
-		reload.addEventListener("click", () => {
-			location.reload();
-		});
-	},
-	complete: () => console.log("Fin du traitement")
-})
+	)
+).subscribe((imgLink) => image.src = imgLink)
