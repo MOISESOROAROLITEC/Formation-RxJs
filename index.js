2ac6774440b92e981a8915ef7e879aebf3e27d86
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-05-23 10:05:47 +0000

    paiement of expected monthly

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-import { combineLatest, from, fromEvent, interval, merge } from "rxjs";
-import { combineLatestWith, delay, map } from "rxjs/operators";
+import { combineLatestWith, fromEvent, map } from "rxjs";
 
-const click$ = fromEvent(document, "click");
-const keyup$ = fromEvent(document, "keyup");
+const amount = document.getElementById("amount");
+const interest = document.getElementById("interest");
+const duration = document.getElementsByName("duration");
+const mounthlyPaementElement = document.getElementById("mounthly-paement");
 
+const amount$ = fromEvent(amount, "change");
 
-click$.pipe(
-	combineLatestWith(keyup$),
-	map(([e1, e2]) => `first element is : ${e1.pointerType}, seconde element is : ${e2.code}`)
-).subscribe(console.log)
+function getValueFromElement(element) {
+	return fromEvent(element, 'change').pipe(
+		map(el => el.target.value)
+	)
+}
+
+function calculeMonthlyPaiement(amount, interest, duration) {
+	const calculateInterest = interest / 1200;
+	const total = amount * calculateInterest / (1 - (Math.pow(1 / (1 + calculateInterest), duration)));
+	return total.toFixed(2);
+}
+
+amount$.pipe(
+	map(el => el.target.value),
+	combineLatestWith(
+		getValueFromElement(interest),
+		getValueFromElement(duration)
+	),
+	map(el => ({ amount: el[0], interest: el[1], duration: el[2] }))
+).subscribe(datas => {
+	const mounthlyPaement = calculeMonthlyPaiement(datas.amount, datas.interest, datas.duration)
+	mounthlyPaementElement.innerHTML = mounthlyPaement;
+});
+
